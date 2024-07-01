49436d7d258277ecf551a60fc23f0ad78df21fc9
Erfan Naeini <68643038+erfannna@users.noreply.github.com>
2024-07-01 14:52:48 +0330

    Update tasks.py
    periodic tasks

--- account/tasks.py
+++ account/tasks.py
@@ -23,6 +23,9 @@ sms = Client(SMS_APIKEY)
 
 # app = Celery()
 
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(60.0, monitor_services.s(), name='check every 60sec')
 
 @shared_task
 def monitor():
@@ -84,63 +87,4 @@ def find_ticket(serv, class_name):
                 serv.passengers.remove(passenger)
             except HTTPError:
                 pass
-
-
-@app.task(bind=True)
-def monitoring(self):
-    services = Service.objects.filter(expired=False)
-
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.set_capability('browserless:token', 'ZhnpIT8gwh6-az6tg0k')
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
-
-    driver = webdriver.Remote(
-        command_executor='https://khcbus-webdriver.liara.run/webdriver',
-        options=chrome_options
-    )
-
-    for serv in services:
-        if serv.name == "اصفهان به خوانسار":
-            class_name = CLASS_NAME_ISF
-        else:
-            class_name = CLASS_NAME_KHC
-
-        driver.get(serv.checking_url)
-
-        wait = WebDriverWait(driver, 10)
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
-
-        if class_name == CLASS_NAME_KHC:
-            elements = []
-            el = driver.find_elements(By.CLASS_NAME, class_name)
-            for e in el:
-                elements.append(e.find_element(By.TAG_NAME, 'span'))
-        else:
-            elements = driver.find_elements(By.CLASS_NAME, class_name)
-
-        free_chairs = 0
-        i = 1
-        for element in elements:
-            if element.text != '0' and element.text != '۰':
-                print(f"BUS {i} - Free chairs: {element.text}")
-                free_chairs += 1
-            i += 1
-
-        if free_chairs != 0:
-            date = date2jalali(serv.datetime).strftime('%y/%m/%d')
-            for passenger in serv.passengers.all():
-                try:
-                    message_id = sms.send_pattern(
-                        SMS_PATTERN_CODE,
-                        SMS_SENDER,
-                        f"+98{passenger.phone_number}",
-                        {"name": f"{passenger.first_name}",
-                         "destination": f"{serv.name}",
-                         "date": f"{date}"
-                         },
-                    )
-                    serv.passengers.remove(passenger)
-                except HTTPError:
-                    pass
-    driver.quit()
+                
